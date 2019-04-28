5089baead0a30ec9d6c965ac0ab757fc23d41971
AndreaMyshkn <atrkvsk@hotmail.com>
2019-04-28 01:26:48 -0500

    módulo highway

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -16064,7 +16064,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52038" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52373" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
